53347c8a6cba37d082de9fb2334e7a7598e1a468
Osmar Domingos <osdomingos@gmail.com>
2025-03-14 16:09:35 -0300

    Correction

--- myApp.js
+++ myApp.js
@@ -1,37 +1,29 @@
-require('dotenv').config()
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
-/*
+const absolutePath = __dirname + "/views/index.html";
 app.get("/", (req, res) => {
-    res.send("Hello Express")
-})
-*/
+    res.sendFile(absolutePath);
+});
 
-const absolutePath = __dirname + "/views/index.html"
-
-app.get("/", (req, res) => {
-    res.sendFile(absolutePath)
-})
-
-const absolutePath2 = __dirname + "/public"
-
+const absolutePath2 = __dirname + "/public";
 app.use("/public", express.static(absolutePath2));
 
 app.get('/json', (req, res) => {
-    let response = ''
-    if (process.env.MESSAGE_STYLE == "uppercase") {
-        response = 'Hello json'.toUpperCase()
+    let response = '';
+    
+    // Verificar o valor de process.env.MESSAGE_STYLE
+    console.log("MESSAGE_STYLE:", process.env.MESSAGE_STYLE);
+    
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        response = 'Hello json'.toUpperCase();
     } else {
-        response = 'Hello json'
-    } 
-    res.json({"message": response})
-})
-
-
-
-
-
+        response = 'Hello json';
+    }
+    
+    res.json({"message": response});
+});
 
 
 
