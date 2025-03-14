5d7a9a5dd3161db64cee48502354b2e4b4008b8f
Osmar Domingos <osdomingos@gmail.com>
2025-03-14 15:18:23 -0300

    First file send

--- myApp.js
+++ myApp.js
@@ -1,8 +1,16 @@
 let express = require('express');
 let app = express();
 
+/*
 app.get("/", (req, res) => {
     res.send("Hello Express")
+})
+*/
+
+const absolutePath = __dirname + "/views/index.html"
+
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath)
 })
 
 
