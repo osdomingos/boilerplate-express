1e3f6b26379cd8b9048e7552a18898d05466cfc8
Osmar Domingos <osdomingos@gmail.com>
2025-03-14 21:25:23 -0300

    Creating a chained  middleware function 1

--- myApp.js
+++ myApp.js
@@ -21,9 +21,19 @@ app.get('/json', (req, res) => {
 })
 */
 
+/*
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next()
+})
+*/
+
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString()
+
+    next()
+}, (req, res) => {
+    res.send({time: req.time})
 })
 
 
