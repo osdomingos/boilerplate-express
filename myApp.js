f8455fd3610ded44d0a44a9b8c0cc690c8a070a6
Osmar Domingos <osdomingos@gmail.com>
2025-03-14 21:02:50 -0300

    AAWSQWDQWD

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ app.get('/json', (req, res) => {
     if (process.env["MESSAGE_STYLE"] == 'uppercase') {
         res.json({ "message": "HELLO JSON" })
     } else {
-        res.json({ "message": "Hello json" })
+        res.json({ "message": "HELLO JSON" })
     }
 })
 
